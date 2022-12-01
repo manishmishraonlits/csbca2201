bccb529486bce9d19ef49f930e0a34590961ceaa
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2022-12-01 09:06:03 +0530

    20221130

---
+++ cpp/sum.cpp
@@ -0,0 +1,21 @@
+/*
+AIM: Accept two integers and find their sum
+Date: 20221130
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cout << "Enter a number: ";
+	cin >> a;				//input
+	cout << "Enter another number: ";
+	cin >> b;
+	c = a + b;
+	cout << "The sum is " << c << endl;		//output
+}
+
+/*
+	BJarne Stroustrup
+*/
